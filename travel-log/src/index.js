e03f46aef99e2634306eb25db551779614d211e6
Eden Brunner <eden.brunner@gmail.com>
2025-06-25 23:55:29 -0400

    Adding middlewear to clean up code

--- travel-log/src/index.js
+++ travel-log/src/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const middlewares = require('./middlewares');
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
@@ -16,21 +18,8 @@ app.get('/',(req, res) => {
     });
 });
 
-app.use((req, res, next) => {
-    const error = new Error(`Not found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-});
-
-//eslint-disable-next-line no-unused-vars 
-app.use((error, req, res, next) => {
-    const statusCode = req.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-        message: error.message,
-        stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
-    });
-});
+app.use(middlewares.notFound);
+app.use(middlewares.ErrorHandler);
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
